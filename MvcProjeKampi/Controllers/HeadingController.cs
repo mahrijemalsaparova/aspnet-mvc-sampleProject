4710b0a4278567978243ced4cbdc277170ed9710
mahrı saparova <mahrijemal.saparova@gmail.com>
2022-09-12 00:00:52 +0300

    last

---
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HeadingController : Controller
+    {
+        HeadingManager headinManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var headingValues = headinManager.GetList();
+            return View(headingValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in categoryManager.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+
+            ViewBag.vlc = valuecategory;
+            return View();  
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            headinManager.HeadingAdd(heading);
+            return RedirectToAction("Index");
+        }
+    }
+}
