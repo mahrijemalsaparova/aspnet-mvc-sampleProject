2decb97c05fcb3e7cac5e1f31e75a32b3481d522
mahrijemalsaparova <mahrijemal.saparova@gmail.com>
2022-09-23 17:19:41 +0300

    cm

---
+++ MvcProjeKampi/Controllers/MessageController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class MessageController : Controller
+    {
+        // Bize gelen mesajların listelenmesi.
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        public ActionResult Inbox()
+        {
+            var messageList = messageManager.GetListInbox();
+            return View(messageList);
+        }
+
+        public ActionResult SendBox()
+        {
+            var messageList = messageManager.GetListSendbox();
+            return View(messageList);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message message)
+        {
+            return View();
+        }
+
+    }
+}
