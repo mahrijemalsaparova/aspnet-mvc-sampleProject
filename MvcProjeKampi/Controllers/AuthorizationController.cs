303604e43dbbcf378aba12cf6f0cdf2a61638888
mahrijemalsaparova <mahrijemal.saparova@gmail.com>
2022-09-30 00:22:44 +0300

    96 Yetkilendirme Düzenlemeleri

---
+++ MvcProjeKampi/Controllers/AuthorizationController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        AdminManager adminManager = new AdminManager(new EfAdminDal());
+        public ActionResult Index()
+        {
+            var adminValues = adminManager.GetList();
+            return View(adminValues);
+        }
+    }
+}
